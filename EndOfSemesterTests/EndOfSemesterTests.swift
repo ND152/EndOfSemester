04b4b36e00d15aeb5c95e8c9e8b5d4719d768c2a
ND152 <100152@swcsd.us>
2024-11-13 09:37:12 -0500

    Initial Commit

---
+++ EndOfSemesterTests/EndOfSemesterTests.swift
@@ -0,0 +1,17 @@
+//
+//  EndOfSemesterTests.swift
+//  EndOfSemesterTests
+//
+//  Created by Noor Dene on 11/13/24.
+//
+
+import Testing
+@testable import EndOfSemester
+
+struct EndOfSemesterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
